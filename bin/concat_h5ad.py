d65eca168bb8ba02bc08dbcaced1b0211311c14f
Nicolás Aira <59507281+nicoaira@users.noreply.github.com>
2024-07-11 18:40:23 +0200

    fixed int in join method

--- bin/concat_h5ad.py
+++ bin/concat_h5ad.py
@@ -17,8 +17,8 @@ def read_samplesheet(samplesheet):
     # samplesheet may contain replicates, when it has,
     # group information from replicates and collapse with commas
     # only keep unique values using set()
-    df = df.groupby(["sample"]).agg(lambda column: ",".join(set(column)))
-
+    df = df.groupby(["sample"]).agg(lambda column: ",".join(set(str(x) for x in column)))
+    
     return df
 
 
